968d3024271565d7a12f7dfb8a5f54d50a26e8de
Redblock6 <37982990+Redblock6YT@users.noreply.github.com>
2024-01-04 21:42:34 -0500

    day 2
    - added why us section
    - adjusted citations
    - adjusted colors
    - lots of presentation work

--- web/pages/_document.js
+++ web/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body style={{backgroundColor: "#ffecd3ff", overflowX: "hidden"}}>
+      <body style={{backgroundColor: "#ffe7bfff", overflowX: "hidden"}}>
         <Main />
         <NextScript />
       </body>
--- web/pages/index.js
+++ web/pages/index.js
@@ -52,24 +52,37 @@ export default function Home() {
               <div id="hero" className={styles.hero}>
                 <img className={styles.fullycenter} src="shelter.jpg" style={{ width: "100%", height: "100%", objectFit: "cover", borderRadius: "25px" }} />
                 <h1 style={{ width: "80%", textAlign: "center", fontSize: "60px", textShadow: "0 0 20px BLACK", color: "white" }} className={[styles.header, styles.fullycenter].join(" ")}>Food and shelter for all in the DMV</h1>
-                <p className={styles.font} style={{position: "absolute", bottom: "10px", left: "10px", color: "#ffffffa6", margin: "0"}}>DLR Group "Exterior view of a section the Triumph with window covered facade illuminated at night with interior rooms visible" DLR Group, 2020, https://www.dlrgroup.com/work/the-triumph-ward-8/</p>
               </div>
             </div>
 
             <div id="innerContent" style={{ padding: "15px 0px" }}>
+              <p className={styles.font} style={{ color: "white", margin: "0", position: "relative", zIndex: "99", padding: "0px 15px" }}>DLR Group "Exterior view of a section the Triumph with window covered facade illuminated at night with interior rooms visible" DLR Group, 2020, <a href="https://www.dlrgroup.com/work/the-triumph-ward-8/">https://www.dlrgroup.com/work/the-triumph-ward-8/</a></p>
               <div id="currentEvents" style={{ marginTop: "15px" }}>
                 <h3 className={styles.header} style={{ color: "black", marginLeft: "20px", marginBottom: "10px" }}>Current Events</h3>
                 <div id="currentEventsList">
                   <div className={styles.item}>
-                    <p style={{color: "rgba(0, 0, 0, 0.300)", margin: "0", width: "100%", textAlign: "center"}} className={styles.fullycenter}>No current events to show</p>
+                    <p style={{ color: "rgba(0, 0, 0, 0.300)", margin: "0", width: "100%", textAlign: "center" }} className={styles.fullycenter}>No current events to show</p>
                   </div>
                 </div>
               </div>
               <div className={styles.divider}></div>
-              <h3>Why Us?</h3>
-              <p>
-
-              </p>
+              <div className={styles.doublegrid}>
+                <div>
+                  <h3 className={styles.header} style={{ color: "black", marginLeft: "20px", marginBottom: "10px" }}>Why Us?</h3>
+                  <p className={styles.description} style={{ fontSize: "25px", padding: "0px 20px" }}>
+                    Our mission is to save lives and create a thriving community by ensuring that everyone in the DMV has food and shelter. We believe in the power of addressing these basic needs comprehensively, as not only a means of survival but also as a building block to create healthier, happier lives. Join us to become a vital part of our movement and directly impact the community.
+                  </p>
+                  <div className={styles.doublegrid} style={{maxWidth: "600px", margin: "auto"}}>
+                    <button className={styles.button} style={{margin: "auto"}}>Support our cause</button>
+                    <button className={styles.button} style={{margin: "auto"}}>Join our cause</button>
+                  </div>
+                </div>
+                <div style={{position: "relative"}}>
+                  <img className={styles.blurredHero} alt="people laying on beds in a homeless shelter" src="shelterinside.jpg" style={{ objectFit: "cover", height: "100%", width: "100%", borderRadius: "25px" }}></img>
+                  <img alt="people laying on beds in a homeless shelter" src="shelterinside.jpg" style={{ objectFit: "cover", height: "100%", width: "100%", borderRadius: "25px", zIndex: "20", position: 'relative' }}></img>
+                  <p className={styles.font} style={{ color: "white", margin: "0", position: "relative", zIndex: "99", padding: "0px 15px" }}>NC 211 "people laying on beds in a homeless shelter" NC 211, 4 October 2022, <a href="https://nc211.org/shelters/">https://nc211.org/shelters/</a></p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
