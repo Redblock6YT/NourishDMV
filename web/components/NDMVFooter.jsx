1393e44652cf922660d57dd576c56d607029de1d
Redblock6 <37982990+Redblock6YT@users.noreply.github.com>
2024-01-06 21:32:44 -0500

    day 3
    forgot to commit yesterday
    
    - started working on footer with citation prop to make it easy to add citations for each page or no citations
    - added get in touch section
    - added make a difference in your community section

---
+++ web/components/NDMVFooter.jsx
@@ -0,0 +1,39 @@
+import styles from '@/styles/Home.module.css'
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
+
+export default function Footer(props) {
+    const [elements, setElements] = useState([]);
+    console.log(props.citations)
+
+    useEffect(() => {
+        if (props.citations != null) {
+            const newElements = props.citations.map(citation => (
+                <div key={citation.name}>
+                    <h3>{citation.name}</h3>
+                    <p>{citation.citation}</p>
+                </div>
+            ));
+            setElements(newElements);
+        }
+    }, [props.citations]);
+
+
+    return (
+        <>
+            <div className={[styles.doublegrid, styles.footer, styles.font].join(" ")}>
+                <div>
+                    <Image src="logo.svg" alt="NourishDMV Logo" height={45} width={200} />
+                </div>
+                {(props.citations != null) ? (
+                    <div id="citationsdiv">
+                        <h1>Citations</h1>
+                        {elements}
+                    </div>
+                ) : () => { }
+                }
+            </div>
+        </>
+
+    );
+}
--- web/pages/index.js
+++ web/pages/index.js
@@ -4,6 +4,7 @@ import styles from '@/styles/Home.module.css'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import anime from 'animejs';
+import Footer from '@/components/NDMVFooter.jsx'
 
 export default function Home() {
   const router = useRouter();
@@ -11,6 +12,7 @@ export default function Home() {
   useEffect(() => {
     if (router.isReady) {
       //once the page is fully loaded, play the splashscreen outro
+      window.scrollTo(0, 0);
       setTimeout(() => {
         document.getElementById("splashscreenOutro").play();
         anime({
@@ -56,7 +58,6 @@ export default function Home() {
             </div>
 
             <div id="innerContent" style={{ padding: "15px 0px" }}>
-              <p className={styles.font} style={{ color: "white", margin: "0", position: "relative", zIndex: "99", padding: "0px 15px" }}>DLR Group "Exterior view of a section the Triumph with window covered facade illuminated at night with interior rooms visible" DLR Group, 2020, <a href="https://www.dlrgroup.com/work/the-triumph-ward-8/">https://www.dlrgroup.com/work/the-triumph-ward-8/</a></p>
               <div id="currentEvents" style={{ marginTop: "15px" }}>
                 <h3 className={styles.header} style={{ color: "black", marginLeft: "20px", marginBottom: "10px" }}>Current Events</h3>
                 <div id="currentEventsList">
@@ -66,27 +67,52 @@ export default function Home() {
                 </div>
               </div>
               <div className={styles.divider}></div>
-              <div className={styles.doublegrid}>
+              <div className={styles.doublegrid} style={{ gridTemplateColumns: "1.2fr 0.8fr" }}>
                 <div>
                   <h3 className={styles.header} style={{ color: "black", marginLeft: "20px", marginBottom: "10px" }}>Why Us?</h3>
                   <p className={styles.description} style={{ fontSize: "25px", padding: "0px 20px" }}>
                     Our mission is to save lives and create a thriving community by ensuring that everyone in the DMV has food and shelter. We believe in the power of addressing these basic needs comprehensively, as not only a means of survival but also as a building block to create healthier, happier lives. Join us to become a vital part of our movement and directly impact the community.
                   </p>
-                  <div className={styles.doublegrid} style={{maxWidth: "600px", margin: "auto"}}>
-                    <button className={styles.button} style={{margin: "auto"}}>Support our cause</button>
-                    <button className={styles.button} style={{margin: "auto"}}>Join our cause</button>
-                  </div>
                 </div>
-                <div style={{position: "relative"}}>
+                <div style={{ position: "relative" }}>
                   <img className={styles.blurredHero} alt="people laying on beds in a homeless shelter" src="shelterinside.jpg" style={{ objectFit: "cover", height: "100%", width: "100%", borderRadius: "25px" }}></img>
                   <img alt="people laying on beds in a homeless shelter" src="shelterinside.jpg" style={{ objectFit: "cover", height: "100%", width: "100%", borderRadius: "25px", zIndex: "20", position: 'relative' }}></img>
-                  <p className={styles.font} style={{ color: "white", margin: "0", position: "relative", zIndex: "99", padding: "0px 15px" }}>NC 211 "people laying on beds in a homeless shelter" NC 211, 4 October 2022, <a href="https://nc211.org/shelters/">https://nc211.org/shelters/</a></p>
+                </div>
+              </div>
+              <div className={styles.divider}></div>
+              <h3 className={styles.header} style={{ color: "black", marginBottom: "20px", textAlign: "center" }}>Make a difference in <a style={{ backgroundColor: "#fbac29ff" }}>your community</a></h3>
+              <div className={styles.doublegrid} style={{ margin: "auto" }}>
+                <button className={styles.button} style={{ margin: "auto", height: "300px", width: "100%", backgroundColor: "#f66d4bff", marginBottom: "15px", fontSize: "40px", fontWeight: "bold" }}>Support our cause</button>
+                <button className={styles.button} style={{ margin: "auto", height: "300px", width: '100%', backgroundColor: "#fbe85dff", marginBottom: "15px", fontSize: "40px", fontWeight: "bold" }}>Join our cause</button>
+              </div>
+              <div className={styles.divider}></div>
+              <h3 className={styles.header} style={{ color: "black", marginBottom: "20px", textAlign: "center" }}>Get in touch</h3>
+              <div className={styles.doublegrid}>
+                <div className={styles.item} style={{ margin: "auto", marginRight: "0px" }}>
+
+                </div>
+                <div className={styles.item} style={{ margin: "auto", marginLeft: "0px" }}>
+
+                </div>
+                <div className={styles.item} style={{ margin: "auto", marginRight: "0px" }}>
+
+                </div>
+                <div className={styles.item} style={{ margin: "auto", marginLeft: '0px' }}>
+
                 </div>
               </div>
             </div>
           </div>
+          <Footer citations={[
+            {
+              name: "Image 1",
+              citation: `DLR Group "Exterior view of a section the Triumph with window covered facade illuminated at night with interior rooms visible" DLR Group, 2020, https://www.dlrgroup.com/work/the-triumph-ward-8/`
+            }, {
+              name: "Image 2",
+              citation: `NC 211 "people laying on beds in a homeless shelter" NC 211, 4 October 2022, https://nc211.org/shelters/`
+            }
+          ]} />
         </div>
-
         <video id="splashscreenOutro" muted className={styles.splashScreen}><source src="helpsoutro.mp4" type="video/mp4" /></video>
       </main>
     </>
