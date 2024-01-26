cee91f5bf055552b7a07eb4a4628da9a0541098e
Redblock6 <37982990+Redblock6YT@users.noreply.github.com>
2024-01-26 11:43:20 -0500

    change api port again..

--- web/pages/index.js
+++ web/pages/index.js
@@ -90,7 +90,7 @@ export default function Home() {
 
     axios({
       method: "get",
-      url: "http://nourishapi.rygb.tech:8445/getEvents"
+      url: "http://nourishnourishapi.rygb.tech:8443/getEvents"
     }).then((res) => {
       if (res.status == 200) {
         console.log("got events")
