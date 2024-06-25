1ffd9420a1d2b470e1f2cb47619dd701c1ee05ce
Redblock6 <37982990+Redblock6YT@users.noreply.github.com>
2024-06-24 22:18:25 -0400

    Day 73
    - clicking the logo on the dashboard should take you to the homepage

--- web/pages/dash.js
+++ web/pages/dash.js
@@ -2356,7 +2356,7 @@ export default function Dash() {
                             <div id="sidebarInteractions">
                                 <div style={{ display: "flex", margin: "auto", marginTop: "5px", alignItems: "center", width: "calc(100% - 15px)" }}>
                                     <button className={[styles.sidebarbutton, styles.hover].join(" ")} style={{ width: "60px", height: "40px", borderRadius: "20px", marginLeft: "10px", marginRight: '5px' }} onClick={() => toggleSidebar()} id="openCloseSidebarAcc"><span className={["material-symbols-rounded", styles.sidebarButtonIcon].join(" ")} style={{ fontSize: "20px" }}>{(sidebarOpen) ? "left_panel_close" : "left_panel_open"}</span></button>
-                                    <Image src="logo.svg" alt="NourishDMV Logo" height={35} width={(mobile) ? 190 : 190} />
+                                    <Image src="logo.svg" alt="NourishDMV Logo" onClick={() => push("/")} height={35} width={(mobile) ? 190 : 190} />
                                 </div>
                                 <div className={styles.divider} style={{ marginTop: "5px", marginBottom: "0px" }}></div>
                                 <div className={styles.innerSidebar}>
