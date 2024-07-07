b74e6260519bbb203dafead84ec0187ed16f2f9b
Redblock6 <37982990+Redblock6YT@users.noreply.github.com>
2024-07-07 19:56:29 -0400

    add Google Analytics 4

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,5 +1,18 @@
 import '@/styles/globals.css'
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Script async src="https://www.googletagmanager.com/gtag/js?id=G-4WN2D1XMMP"></Script>
+    <Script>
+      {
+        `window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-4WN2D1XMMP');`
+      }
+    </Script>
+    <Component {...pageProps} />
+  </>
 }
